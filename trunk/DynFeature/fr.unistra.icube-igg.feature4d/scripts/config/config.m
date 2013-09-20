5c0192a18d53fa37bab48e9e5ef6dc11b9f9e281
iirthw <iirthw@76f3328c-faed-393e-af48-33138a508dee>
2013-09-20 15:22:42 +0000

    Added possibility to use two different functions for search of maxima in scale and space-time

--- trunk/DynFeature/fr.unistra.icube-igg.feature4d/scripts/config/config.m
+++ trunk/DynFeature/fr.unistra.icube-igg.feature4d/scripts/config/config.m
@@ -11,7 +11,7 @@ function param = config(data_name)
 % Set it to TRUE in order to recompute every step from scratch including full
 % pre-processing: that might be of use when changing input data sets, for
 % example
-param.recompute_all = false; 
+param.recompute_all = true; 
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
 param.visualize_strain = true; % export .PLY files with color-coded vertex strains
 % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % %
@@ -24,11 +24,17 @@ param.data_file = [param.data_folder data_name '.mat'];
 % Path to file which contains n-ring pre-computed neighbourhoods
 param.window_file = [param.data_folder param.data_name '-window.mat'];
 
+% Function to define local maxima in "scale space" domain (inter-octave)
+param.response_fn_scale = @diff_of_gauss;
+% Function to define local maxima in "space-time" domain (intra-octave)
+%param.response_fn_spacetime = @pure_strain;
+param.response_fn_spacetime = @diff_of_gauss;
+
 % Define space-time window
 param.window = boxcar_window(1, 1); % 1-ring neigh. both in space and time
 
 % Number of frames to consider in animation sequence
-%param.n_f = 80;
+param.n_f = 40;
 
 % Linear scale space representation)
 param.smooth_num = 10; % Total number of octaves: smooth_num x smooth_num (recommended to be < 50) 

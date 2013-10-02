08938f290559d17890b52679d84600f2fc731819
iirthw <iirthw@76f3328c-faed-393e-af48-33138a508dee>
2013-10-02 14:45:40 +0000

    Added some nice post-processing scripts to help in the analysis of the results.

---
+++ trunk/DynFeature/fr.unistra.icube-igg.feature4d/scripts/postprocess/postprocess.m
@@ -0,0 +1,27 @@
+% Data set name
+param.data_name = 'horse';
+
+cd('..');
+% Load precomputed data
+
+% Threshold for feature response
+eps = 0.4;
+
+% Filter raw feature point set
+featpts_filtered = postprocess_featpts( featpts, eps )
+featpts = featpts_filtered;
+
+% Modify the directory for export
+param.data_name = [param.data_name '_' num2str(eps)];
+
+% Export all the frames OBJ files augmented with spheres on feature point
+% locations
+vertices_with_spheres = export_feature_vis( D_, objseq, param, featpts );
+
+% Export extracted features (raw featpts i.e. without filtering)
+export_featpts_mat(featpts, param);
+
+% Export strain color during the animations
+export_anim_color( feature_response, vertices_with_spheres, param );
+
+cd('postprocess');

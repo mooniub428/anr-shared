0f6f06fbb2d315ac338baa74bcad3aff3b211950
iirthw <iirthw@76f3328c-faed-393e-af48-33138a508dee>
2013-10-02 13:03:18 +0000

    Post-processing function to filter out feature points.

---
+++ trunk/DynFeature/fr.unistra.icube-igg.feature4d/scripts/postprocess/postprocess_featpts.m
@@ -0,0 +1,20 @@
+function [ featpts_filtered ] = postprocess_featpts( featpts, eps )
+
+% number of feature pts
+n = numel(featpts);
+
+% Init a set of filtered feature points
+featpts_filtered = [];
+
+% Iterate over features and filter out those with a feature respose less
+% then pre-defined threshold "eps"
+for i = 1 : n
+    feat = featpts{i};
+    f_response = feat(3);
+    if(f_response > eps)
+        featpts_filtered = [featpts_filtered; feat];
+    end % if
+end % for
+
+end % function
+

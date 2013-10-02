a0b2ee5a7c8f5560d2bd07854cbe482aee74618e
iirthw <iirthw@76f3328c-faed-393e-af48-33138a508dee>
2013-10-02 14:53:40 +0000

    Minor bug fix in benchmark due to inappropriate placement of "clear" statement

--- DynFeature/fr.unistra.icube-igg.feature4d/scripts/benchmark.m
+++ DynFeature/fr.unistra.icube-igg.feature4d/scripts/benchmark.m
@@ -9,21 +9,21 @@ n = numel(datasets);
 % 1
 % Compute featpts with param.do_scale_norm == true
 for i = 1 : n
-    clear;
     try
         main_f(cell2mat(datasets(i)), true);
     catch err
         err % just print the error to console
     end
+    clear;
 end % for
 
 % 2
 % Compute featpts with param.do_scale_norm == false
 for i = 1 : n
-    clear;
     try
         main_f(cell2mat(datasets(i)), false);
     catch err
         err % just print the error to console
     end
+    clear;
 end % for

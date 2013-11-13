88bbb916d137ad16eb9f6f917fc1c185a203513c
iirthw <iirthw@gmail.com>
2013-11-13 14:48:54 +0000

    Commented saving response temporarily

--- scripts/output/export_IP_index.m
+++ scripts/output/export_IP_index.m
@@ -2,9 +2,10 @@ function [] = export_IP_index(export_dir, VIndex, J, response, IP)
 
 IPFileName = [export_dir 'IP.txt'];
 
-R = response';
+R = zeros(numel(J), 1);
+%R = response';
 
-R = R(IP' > 0);
+%R = R(IP' > 0);
 
 IPIndex = [J VIndex R];
 save(IPFileName, 'IPIndex', '-ascii');

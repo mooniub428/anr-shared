22919c05ff9dc2a318821882c8568268495951ab
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2013-11-25 09:51:55 +0000

    Use new T computation in time smoothing

--- branches/interest-point-fast/scripts/pyramid/do_time_smoothing.m
+++ branches/interest-point-fast/scripts/pyramid/do_time_smoothing.m
@@ -1,21 +1,12 @@
-function [ pyramid ] = do_time_smoothing(pyramid, D_, tau)
+function [ pyramid ] = do_time_smoothing(pyramid, D_, tau, wt)
+% wt - time window
 
 n_v = size(D_, 1);
 n_f = size(D_, 2);
 
 % T is 1-ring remporal filter matrix
 % S(n+1) = S(n)*T
-T = zeros(n_f, n_f);
-n_f = n_f - 1;
-T(1,1) = 1/2;
-T(2,1) = 1/2;
-for k = 2 : n_f
-    T(k-1, k) = 1/3;
-    T(k, k) = 1/3;    
-    T(k+1, k) = 1/3;
-end % for
-T(n_f, n_f+1) = 1/2;
-T(n_f+1, n_f+1) = 1/2;
+T = ring_time(n_f, wt);
 
 tau = tau + 1;
 for i = 2 : tau      

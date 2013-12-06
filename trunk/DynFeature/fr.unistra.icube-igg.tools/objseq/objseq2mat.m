64683d884b50396d7b11dabb941bc26f7ea7b5f7
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2013-12-06 08:56:11 +0000

    Invoke geod executable from Matlab

--- trunk/DynFeature/fr.unistra.icube-igg.tools/objseq/objseq2mat.m
+++ trunk/DynFeature/fr.unistra.icube-igg.tools/objseq/objseq2mat.m
@@ -7,14 +7,21 @@ num_files = numel(listing);
 
 
 % skip '.' and '..' in dir output
+name = listing(3).name;
 next_file = [param.data_folder 'obj/' listing(3).name];
 [vertices, triangles] = read_wobj(next_file);
 V = zeros( size(vertices, 1), 3, (num_files - 2));
 V(:,:,1) = vertices;
 
+% Compute geodesic distance
+if (param.region_deformation)
+    Gd = compute_geod(param.data_folder, param.data_name, next_file, name);
+    objseq.Gd = Gd;
+end % if
+
 parfor i = 4 : num_files
     next_file = [param.data_folder 'obj/' listing(i).name];
-    [vertices, triangles] = read_wobj(next_file);
+    [vertices, ~] = read_wobj(next_file);
     V(:,:, i-2) = vertices;
 end % for
 

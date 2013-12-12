354519a2fc3ceb0cc1768ba8e42a2b945f6e9212
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2013-12-12 16:43:10 +0000

    Discrete gradient approximation using several discrete directional derivatives

---
+++ branches/interest-point-fast/scripts/descriptor/gradient/Grad.m
@@ -0,0 +1,23 @@
+% Discrete gradient approximation using several discrete directional 
+% derivatives [Zaharescu et al., Surface Feature Detection and Description
+% with Application to Mesh Matching]
+%
+function [g] = Grad(F, U, W)
+
+Dd = DirectDerivative(F, U);
+
+x = U(1, :);
+n = size(U, 1) - 1;
+
+g = [0.0 0.0 0.0];
+if(W == 0) W = ones(n + 1); end % if
+
+for i = 1 : n
+    y = U(i, :);
+    g = g + W(i + 1) * Dd(i) * (y - x);
+end % for
+
+g = g';
+
+end % function
+

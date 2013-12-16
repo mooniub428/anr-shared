78ae006c95a81de16d7c6091138c4e64faa628dd
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2013-12-16 17:07:10 +0000

    Change basis routine

--- branches/interest-point-fast/scripts/descriptor/hog/HistogramOfGradient.m
+++ branches/interest-point-fast/scripts/descriptor/hog/HistogramOfGradient.m
@@ -1,4 +1,11 @@
 function [H] = HistogramOfGradient(V, Gr)
 
+num_bins = 36;
+
+hist_dim = size(Gr, 3);
+n = size(Gr, 1);
+
+H = zeros(n, hist_dim);
+
 end % function
 
---
+++ branches/interest-point-fast/scripts/descriptor/orientation/RotAngle.m
@@ -0,0 +1,21 @@
+function [angle] = RotAngle(e_prime)
+
+x = e_prime(1);
+y = e_prime(2);
+
+if(x > 0 && y >= 0) % I Quarter
+    angle = atan(y / x);
+elseif( x < 0 && y >= 0) % II Quarter
+    angle = atan(y / x);
+    angle = pi + angle;
+elseif(x < 0 && y <= 0) % III Quarter
+    angle = atan(y / x);
+    angle = pi + angle;
+elseif(x > 0 && y <= 0) % IV Quarter
+    angle = atan(y / x);
+elseif(x == 0) % Singularity/Zero angle
+    angle = 0.0;
+end % if
+
+end % function
+

5fffda4a559f1dd9cc98b10b755abb6b54f4cbb5
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2013-12-17 09:43:35 +0000

    Generate adjacency in planePatch

--- branches/interest-point-fast/scripts/descriptor/plane/planePatch.m
+++ branches/interest-point-fast/scripts/descriptor/plane/planePatch.m
@@ -1,23 +1,31 @@
-function [G] = planePatch()
+function [X, A] = planePatch()
 
 k = 1;
-X = -k : k;
-Y = -k : k;
+U = -k : k;
+W = -k : k;
 
 d = 1;
 [a b c] = plane([1 0 0]',[0 2 0]', [0 0 1]', d);
 
-G = zeros((k * 2 + 1)^2, 3);
+X = zeros((k * 2 + 1)^2, 3);
+
 for i = 1 : (2 * k + 1)
     for j = 1 : (2 * k + 1)
-        x = X(i);
-        y = Y(j);
+        x = U(i);
+        y = W(j);
         [z] = z4planeXY(a, b, c, d, x, y);
-        G((i - 1) * (2 * k + 1) + j, 1) = x;
-        G((i - 1) * (2 * k + 1) + j, 2) = y;
-        G((i - 1) * (2 * k + 1) + j, 3) = z;
+        X((i - 1) * (2 * k + 1) + j, 1) = x;
+        X((i - 1) * (2 * k + 1) + j, 2) = y;
+        X((i - 1) * (2 * k + 1) + j, 3) = z;                       
     end % for
 end % for
+
+% Generate adjacency matrix
+A = zeros( (k * 2 + 1)^2 );
+i = 5;
+A(i, :) = ones(1, (k * 2 + 1)^2);
+A(:, i) = ones((k * 2 + 1)^2, 1);
+A(i, i) = 0;
 
 end % function
 

1aa992ac64a560f7fdd050f1a3f4af8bef7b51b7
iirthw@gmail.com <iirthw@gmail.com@76f3328c-faed-393e-af48-33138a508dee>
2014-01-04 14:57:01 +0000

    Histogram smoothing

---
+++ branches/interest-point-fast/scripts/descriptor/InterpolateAllHistograms.m
@@ -0,0 +1,31 @@
+function [Histograms] = InterpolateAllHistograms(Histograms)
+    for i = 1 : 8
+        H = InterpolateSingleHistogram(cell2mat(Histograms(i)));
+        Histograms(i) = mat2cell(H);
+    end % for
+end % function
+
+function [H] = InterpolateSingleHistogram(H)
+    [numRows, numCols] = size(H);
+    [row, col, v] = find(H);
+    
+    X = [];
+    H = H / max(max(H));
+    for i = 1 : numRows
+        for j = 1 : numCols
+            numOfSamples = floor(H(i, j) * 30);
+            for k = 1 : numOfSamples
+                X = [X; [i j]];
+            end % for
+        end % for
+    end % for     
+    smoothhist2D(X, 2, [numRows numCols])
+    
+    %x = row;
+    %y = col;    
+    %v = v;
+    %[xq, yq] = meshgrid(1:numCols, 1:numRows);    
+    %Interpolator = TriScatteredInterp(x,y,v,'nearest');
+    %HInterp = Interpolator(xq, yq);
+end % function
+

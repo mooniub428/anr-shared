ccebeea81bd4f654c5dc4cb8c61a2c0e52fd9076
iirthw <iirthw@gmail.com>
2014-01-08 13:27:29 +0000

    Load dependencies on Matlab path in a correct way

--- scripts/load_coretools.m
+++ scripts/load_coretools.m
@@ -1,16 +1,23 @@
 function [ ] = load_coretools( )
 
-addpath('../tools/obj');
-addpath('../tools/export');
-addpath('../tools/objseq');
+addpath('../tools');
+addpath('../tools/3dsmax');
+addpath('../tools/centroid');
 addpath('../tools/color');
+addpath('../tools/config');
+addpath('../tools/export');
+addpath('../tools/geod');
+addpath('../tools/obj');
+addpath('../tools/objseq');
 
 addpath('config');
 addpath('deformation');
+addpath('descriptor');
 addpath('interest');
 addpath('mesh');
 addpath('output');
 addpath('pyramid');
+addpath('vectorfield');  
 
 end % function
 

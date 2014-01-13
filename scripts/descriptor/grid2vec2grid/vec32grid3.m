bc568b789819c02c4bdb4aa61f1e3bab036e4b28
iirthw <iirthw@gmail.com>
2014-01-13 15:55:44 +0000

    Corrected implementation of vec3 to 3d grid conversion

--- scripts/descriptor/grid2vec2grid/vec32grid3.m
+++ scripts/descriptor/grid2vec2grid/vec32grid3.m
@@ -7,9 +7,9 @@ Z = zeros(nx, ny, nz);
 for i = 1 : nx
     for j = 1 : ny
         for k = 1 : nz
-            X(i, j, k) = vector((i - 1) * nx * ny + (j - 1) * ny + k, 1);                                             
-            Y(i, j, k) = vector((i - 1) * nx * ny + (j - 1) * ny + k, 2); 
-            Z(i, j, k) = vector((i - 1) * nx * ny + (j - 1) * ny + k, 3); 
+            X(i, j, k) = vector((i - 1) * ny * nz + (j - 1) * nz + k, 1);                                             
+            Y(i, j, k) = vector((i - 1) * ny * nz + (j - 1) * nz + k, 2); 
+            Z(i, j, k) = vector((i - 1) * ny * nz + (j - 1) * nz + k, 3); 
         end % for
     end % for
 end % for

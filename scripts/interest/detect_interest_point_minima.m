9c038844e5d2d44a473dc2c66c8fcdedec44e783
iirthw <iirthw@gmail.com>
2014-01-21 15:13:44 +0000

    Save correct colors; correct indexing in pyramid construction and interest point detection stages

--- scripts/interest/detect_interest_point_minima.m
+++ scripts/interest/detect_interest_point_minima.m
@@ -1,18 +1,24 @@
-function [IP] = detect_interest_point(response, A, n_v, n_f, sigma, tau, eps, step)
+function [IP] = detect_interest_point_minima(response, A, n_v, n_f, sigma, tau, eps, step)
 
 disp('Detect interest points ::');
 
-max_tau = tau;
-max_sigma = sigma;
-sigma = sigma + 1;
-tau = tau + 1;
-   
+param = config('');
+
+if(param.correct_indexing)
+    max_tau = tau - 1;
+    max_sigma = sigma - 1;
+else
+    tau = tau - 1;
+    sigma = sigma - 1;
+    max_tau = tau;
+    max_sigma = sigma;
+end % if
 
 % Interest points
 IP = zeros(n_v * sigma * tau, n_f);
 
-for t_i = 2 : max_tau
-    for s_i = 2 : max_sigma
+for t_i = step + 1 : tau - 1 
+    for s_i = step + 1 : sigma - 1
         %% Extrema in scale
         [L, from_id_, to_id_] = get_at_scale(n_v, response, s_i, t_i, sigma, tau); % Center
         
@@ -28,9 +34,9 @@ for t_i = 2 : max_tau
         [L7, ~] = get_at_scale(n_v, response, s_i+1, t_i-1, sigma, tau); % Right-Up
         [L8, ~] = get_at_scale(n_v, response, s_i, t_i-1, sigma, tau); % Up                        
         
-        B = ((L - L1) <= 0) + ((L - L2) <= 0) + ((L - L3) <= 0) + ...
-            + ((L - L4) <= 0) + ((L - L5) <= 0) + ((L - L6) <= 0) + ...
-            + ((L - L7) <= 0) + ((L - L8) <= 0) + (L <= eps);
+        B = ((L - L1) >= 0) + ((L - L2) >= 0) + ((L - L3) >= 0) + ...
+            + ((L - L4) >= 0) + ((L - L5) >= 0) + ((L - L6) >= 0) + ...
+            + ((L - L7) >= 0) + ((L - L8) >= 0) + (L <= eps);
         
         %% Extrema in space-time
         % 1. Time
@@ -38,7 +44,7 @@ for t_i = 2 : max_tau
         L_next(:, 1) = zeros(1, n_v);
         L_prev = circshift(L, [0 -1]);
         L_prev(:, n_f) = zeros(1, n_v);
-        B = B + ((L - L_prev) <= 0) + ((L - L_next) <= 0);
+        B = B + ((L - L_prev) >= 0) + ((L - L_next) >= 0);
         
                  
         % 2. Space
@@ -47,7 +53,7 @@ for t_i = 2 : max_tau
             adj = adj'; % to column vector          
             
             for f_i = 1 : n_f
-                B(v_i, f_i) = B(v_i, f_i) + (L(v_i, f_i) - max(L(adj, f_i)) <= 0);   
+                B(v_i, f_i) = B(v_i, f_i) + (L(v_i, f_i) - min(L(adj, f_i)) >= 0);   
             end % for            
         end % for        
         
--- scripts/interest/response_extended_DoG.m
+++ scripts/interest/response_extended_DoG.m
@@ -2,15 +2,18 @@ function [response] = response_extended_DoG(pyramid, n_v, n_f, sigma, tau, param
 
 response = (-1) * ones((tau + 1) * (sigma + 1) * n_v, n_f);
 
-step = param.step;
-for t_i = step + 1 : (tau)
-    for s_i = step + 1 : (sigma)
+max_sigma = sigma;
+sigma = sigma + 1;
+max_tau = tau;
+tau = tau + 1;
+for t_i = 2 : max_tau
+    for s_i = 2 : max_sigma
         [S22, from_id, to_id] = get_at_scale(n_v, pyramid, s_i, t_i, sigma, tau); % Left-Up
-        [S11, ~] = get_at_scale(n_v, pyramid, s_i - step, t_i - step, sigma, tau); % Left
-        [S12, ~] = get_at_scale(n_v, pyramid, s_i - step, t_i , sigma, tau); % Left-Bottom        
-        [S21, ~] = get_at_scale(n_v, pyramid, s_i, t_i - step, sigma, tau); % Bottom
+        [S11, ~] = get_at_scale(n_v, pyramid, s_i - 1, t_i - 1, sigma, tau); % Left
+        [S12, ~] = get_at_scale(n_v, pyramid, s_i - 1, t_i , sigma, tau); % Left-Bottom        
+        [S21, ~] = get_at_scale(n_v, pyramid, s_i, t_i - 1, sigma, tau); % Bottom
         
-        feat_response = abs(S22 + S11 - S12 - S21);
+        feat_response = abs(S22 + S11 - S12 - S21);            
         response(from_id : to_id, :) = feat_response;                           
     end % for
 end % for
@@ -19,8 +22,16 @@ end % for
 disp('Saving color..');
 from_id = 1;
 to_id = n_v;
-S00 = pyramid(from_id : to_id, :);
-S00_vector = reshape(S00', numel(S00), 1); % Vectorized form of reference scale S00
+%S00 = pyramid(from_id : to_id, :);
+R11 = get_at_scale(n_v, response, 2, 3, sigma, tau);
+S00 = get_at_scale(n_v, pyramid, 1, 1, sigma, tau);
+S00_vector = zeros(n_v * n_f, 1);
+R11_vector = zeros(n_v * n_f, 1);
+for i = 1 : n_f
+    S00_vector( (i-1) * n_v + 1 : i * n_v) = S00(:, i);
+    R11_vector( (i-1) * n_v + 1 : i * n_v) = R11(:, i);
+end % for
+%S00_vector = reshape(S00', numel(S00), 1); % Vectorized form of reference scale S00
 C = produce_color(S00_vector) / 255.0;
 
 % Create export directory if it does not exist
@@ -28,7 +39,11 @@ enforce_existence('../debug/');
 export_dir = ['../debug/' param.data_name '/'];
 enforce_existence(export_dir);
 
-CFileName = [export_dir 'Colors.txt'];
+CFileName = [export_dir 'ColorsVertexStrain.txt'];
+save(CFileName, 'C', '-ascii');
+
+C = produce_color(R11_vector) / 255.0;
+CFileName = [export_dir 'ColorsVertexResponse.txt'];
 save(CFileName, 'C', '-ascii');
 
 
